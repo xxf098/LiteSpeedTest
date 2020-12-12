6bb74ef8a68e6e5ce6f15656a116f9629f275339
xxf098 <ufxmeng@gmail.com>
2020-12-12 08:27:11 -0500

    VmessLinkToVmessConfigIP

--- config/vmess.go
+++ config/vmess.go
@@ -3,7 +3,6 @@ package config
 import (
 	"encoding/base64"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
 	"regexp"
 	"strconv"
@@ -50,18 +49,22 @@ type RawConfig struct {
 }
 
 type VmessConfig struct {
-	Add      string          `json:"add"`
-	Aid      json.RawMessage `json:"aid"`
-	Host     string          `json:"host"`
-	ID       string          `json:"id"`
-	Net      string          `json:"net"`
-	Path     string          `json:"path"`
-	Port     json.RawMessage `json:"port"`
-	Ps       string          `json:"ps"`
-	TLS      string          `json:"tls"`
-	Type     string          `json:"type"`
-	V        json.RawMessage `json:"v"`
-	Security string          `json:"security"`
+	Add        string          `json:"add"`
+	Aid        json.RawMessage `json:"aid"`
+	Host       string          `json:"host"`
+	ID         string          `json:"id"`
+	Net        string          `json:"net"`
+	Path       string          `json:"path"`
+	Port       json.RawMessage `json:"port"`
+	Ps         string          `json:"ps"`
+	TLS        string          `json:"tls"`
+	Type       string          `json:"type"`
+	V          json.RawMessage `json:"v"`
+	Security   string          `json:"security"`
+	ResolveIP  bool            `json:"resolve_ip"`
+	ServerName string
+	PortInt    int
+	AidInt     int
 }
 
 func RawConfigToVmessOption(config *RawConfig) (*outbound.VmessOption, error) {
@@ -144,10 +147,13 @@ func VmessConfigToVmessOption(config *VmessConfig) (*outbound.VmessOption, error
 		UDP:            false,
 		Network:        "tcp",
 		SkipCertVerify: false,
+		Type:           config.Type,
 	}
-	if ipAddr, err := resolveIP(vmessOption.Server); err == nil && ipAddr != "" {
-		vmessOption.ServerName = vmessOption.Server
-		vmessOption.Server = ipAddr
+	if config.ResolveIP {
+		if ipAddr, err := resolveIP(vmessOption.Server); err == nil && ipAddr != "" {
+			vmessOption.ServerName = vmessOption.Server
+			vmessOption.Server = ipAddr
+		}
 	}
 	if config.TLS == "tls" {
 		vmessOption.TLS = true
@@ -166,6 +172,10 @@ func VmessConfigToVmessOption(config *VmessConfig) (*outbound.VmessOption, error
 }
 
 func VmessLinkToVmessOption(link string) (*outbound.VmessOption, error) {
+	return VmessLinkToVmessOptionIP(link, false)
+}
+
+func VmessLinkToVmessOptionIP(link string, resolveip bool) (*outbound.VmessOption, error) {
 	regex := regexp.MustCompile(`^vmess://([A-Za-z0-9+-=/]+)`)
 	res := regex.FindAllStringSubmatch(link, 1)
 	b64 := ""
@@ -173,7 +183,7 @@ func VmessLinkToVmessOption(link string) (*outbound.VmessOption, error) {
 		b64 = res[0][1]
 	}
 	data, err := base64.StdEncoding.DecodeString(b64)
-	fmt.Println(string(data))
+	// fmt.Println(string(data))
 	if err != nil {
 		return nil, err
 	}
@@ -182,7 +192,48 @@ func VmessLinkToVmessOption(link string) (*outbound.VmessOption, error) {
 	if err != nil {
 		return nil, err
 	}
+	config.ResolveIP = resolveip
 	return VmessConfigToVmessOption(&config)
+}
+
+func VmessLinkToVmessConfigIP(link string, resolveip bool) (*VmessConfig, error) {
+	regex := regexp.MustCompile(`^vmess://([A-Za-z0-9+-=/]+)`)
+	res := regex.FindAllStringSubmatch(link, 1)
+	b64 := ""
+	if len(res) > 0 && len(res[0]) > 1 {
+		b64 = res[0][1]
+	}
+	data, err := base64.StdEncoding.DecodeString(b64)
+	// fmt.Println(string(data))
+	if err != nil {
+		return nil, err
+	}
+	config := VmessConfig{}
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		return nil, err
+	}
+	port, err := rawMessageToInt(config.Port)
+	if err != nil {
+		return nil, err
+	}
+	config.PortInt = port
+	aid, err := rawMessageToInt(config.Aid)
+	if err != nil {
+		return nil, err
+	}
+	config.AidInt = aid
+	if resolveip {
+		if ipAddr, err := resolveIP(config.Add); err == nil && ipAddr != "" {
+			config.ServerName = config.Add
+			if config.Host == "" {
+				config.Host = config.ServerName
+			}
+			config.Add = ipAddr
+
+		}
+	}
+	return &config, nil
 }
 
 func ToVmessOption(path string) (*outbound.VmessOption, error) {
