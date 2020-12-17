4e4af2d1d0954d7da5e82a986f7be9f4be4b2174
xxf098 <ufxmeng@gmail.com>
2020-12-17 01:02:38 -0500

    log

---
+++ log/log.go
@@ -0,0 +1,57 @@
+package log
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	logger    = log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	loggerErr = log.New(os.Stderr, "", log.Ldate|log.Ltime)
+	level     = ERROR
+)
+
+func SetLevel(newLevel LogLevel) {
+	level = newLevel
+}
+
+type Message interface {
+	String() string
+}
+
+// Write a simple logger
+func Write(msg Message) {
+	logger.Print(msg.String() + "\n")
+}
+
+func Info(format string, v ...interface{}) {
+	if INFO < level {
+		return
+	}
+	Print(format, v...)
+}
+
+func Warn(format string, v ...interface{}) {
+	if WARNING < level {
+		return
+	}
+	Print(format, v...)
+}
+
+func Error(format string, v ...interface{}) {
+	if ERROR < level {
+		return
+	}
+	Print(format, v...)
+}
+
+func Print(msg string, args ...interface{}) {
+	m := fmt.Sprintf(msg, args...)
+	logger.Println(m)
+}
+
+func printErr(msg string, args ...interface{}) {
+	m := fmt.Sprintf(msg, args...)
+	loggerErr.Println(m)
+}
