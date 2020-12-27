756967b1f36bfd80d98fca07994c79c60486d782
xxf098 <ufxmeng@gmail.com>
2020-12-27 09:26:20 -0500

    freedom PacketConn

--- tunnel/freedom/conn.go
+++ tunnel/freedom/conn.go
@@ -3,6 +3,7 @@ package freedom
 import (
 	"net"
 
+	"github.com/xxf098/lite-proxy/common"
 	"github.com/xxf098/lite-proxy/tunnel"
 )
 
@@ -12,4 +13,40 @@ type Conn struct {
 
 func (c *Conn) Metadata() *tunnel.Metadata {
 	return nil
+}
+
+type PacketConn struct {
+	*net.UDPConn
+}
+
+func (c *PacketConn) WriteWithMetadata(p []byte, m *tunnel.Metadata) (int, error) {
+	return c.WriteTo(p, m.Address)
+}
+
+func (c *PacketConn) ReadWithMetadata(p []byte) (int, *tunnel.Metadata, error) {
+	n, addr, err := c.ReadFrom(p)
+	if err != nil {
+		return 0, nil, err
+	}
+	address, err := tunnel.NewAddressFromAddr("udp", addr.String())
+	common.Must(err)
+	metadata := &tunnel.Metadata{
+		Address: address,
+	}
+	return n, metadata, nil
+}
+
+func (c *PacketConn) WriteTo(p []byte, addr net.Addr) (int, error) {
+	if udpAddr, ok := addr.(*net.UDPAddr); ok {
+		return c.WriteToUDP(p, udpAddr)
+	}
+	ip, err := addr.(*tunnel.Address).ResolveIP()
+	if err != nil {
+		return 0, err
+	}
+	udpAddr := &net.UDPAddr{
+		IP:   ip,
+		Port: addr.(*tunnel.Address).Port,
+	}
+	return c.WriteToUDP(p, udpAddr)
 }
