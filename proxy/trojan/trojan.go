f045e43df162fa43ac73f8e304a8acb9e48ad53a
xxf098 <ufxmeng@gmail.com>
2020-12-28 21:37:37 -0500

    trojan proxy

---
+++ proxy/trojan/trojan.go
@@ -0,0 +1,45 @@
+package trojan
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	C "github.com/xxf098/lite-proxy/constant"
+	"github.com/xxf098/lite-proxy/outbound"
+	"github.com/xxf098/lite-proxy/tunnel"
+)
+
+type Client struct {
+	ctx    context.Context
+	trojan *outbound.Trojan
+}
+
+func (c Client) DialConn(addr *tunnel.Address, _ tunnel.Tunnel) (net.Conn, error) {
+	meta := &C.Metadata{
+		NetWork:  0,
+		Type:     0,
+		SrcPort:  "",
+		AddrType: int(addr.AddressType),
+		DstPort:  fmt.Sprintf("%d", addr.Port),
+	}
+	switch addr.AddressType {
+	case tunnel.IPv4:
+	case tunnel.IPv6:
+		meta.DstIP = addr.IP
+	case tunnel.DomainName:
+		meta.Host = addr.DomainName
+	}
+	return c.trojan.DialContext(c.ctx, meta)
+}
+
+func (c Client) Close() error {
+	return nil
+}
+
+func NewClient(ctx context.Context, trojan *outbound.Trojan) Client {
+	return Client{
+		ctx:    ctx,
+		trojan: trojan,
+	}
+}
