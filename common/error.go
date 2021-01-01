48283e33d23eb04b28d498e9b7def22f58977746
xxf098 <ufxmeng@gmail.com>
2021-01-01 09:12:23 -0500

    error

---
+++ common/error.go
@@ -0,0 +1,44 @@
+package common
+
+import (
+	"fmt"
+)
+
+type Error struct {
+	info string
+}
+
+func (e *Error) Error() string {
+	return e.info
+}
+
+func (e *Error) String() string {
+	return e.info
+}
+
+func (e *Error) Base(err error) *Error {
+	if err != nil {
+		e.info += " | " + err.Error()
+	}
+	return e
+}
+
+func NewError(info string) *Error {
+	return &Error{
+		info: info,
+	}
+}
+
+func Must(err error) {
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+}
+
+func Must2(_ interface{}, err error) {
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+}
