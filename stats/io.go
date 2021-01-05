c40bcb5dcce7fbf1cbeb56da62d631e64c7296aa
xxf098 <ufxmeng@gmail.com>
2021-01-05 08:31:38 -0500

    StatsConn

--- stats/io.go
+++ stats/io.go
@@ -1,27 +1,53 @@
 package stats
 
 import (
-	"io"
+	"net"
 )
 
-type SizeStatWriter struct {
-	Counter *Counter
-	Writer  io.Writer
+// type SizeStatWriter struct {
+// 	Counter *Counter
+// 	Writer  io.Writer
+// }
+
+// func (s SizeStatWriter) Write(p []byte) (int, error) {
+// 	n, err := s.Writer.Write(p)
+// 	s.Counter.Add(int64(n))
+// 	return n, err
+// }
+
+// type SizeStatReader struct {
+// 	Counter *Counter
+// 	Reader  io.Reader
+// }
+
+// func (s SizeStatReader) Read(p []byte) (int, error) {
+// 	n, err := s.Reader.Read(p)
+// 	s.Counter.Add(int64(n))
+// 	return n, err
+// }
+
+type StatsConn struct {
+	net.Conn
+	CounterDown *Counter
+	CounterUp   *Counter
 }
 
-func (s SizeStatWriter) Write(p []byte) (int, error) {
-	n, err := s.Writer.Write(p)
-	s.Counter.Add(int64(n))
+func (s StatsConn) Read(b []byte) (n int, err error) {
+	n, err = s.Conn.Read(b)
+	s.CounterDown.Add(int64(n))
 	return n, err
 }
 
-type SizeStatReader struct {
-	Counter *Counter
-	Reader  io.Reader
+func (s StatsConn) Write(b []byte) (n int, err error) {
+	n, err = s.Conn.Write(b)
+	s.CounterUp.Add(int64(n))
+	return n, err
 }
 
-func (s SizeStatReader) Read(p []byte) (int, error) {
-	n, err := s.Reader.Read(p)
-	s.Counter.Add(int64(n))
-	return n, err
+func NewStatsConn(c net.Conn) StatsConn {
+	return StatsConn{
+		Conn:        c,
+		CounterDown: DefaultManager.GetCounter(DownProxy),
+		CounterUp:   DefaultManager.GetCounter(UpProxy),
+	}
 }
