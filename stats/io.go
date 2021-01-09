450d9f0a7ab621d1c8aeba6e3a04fdd83205a750
xxf098 <ufxmeng@gmail.com>
2021-01-09 08:29:38 -0500

    StatsPacketConn

--- stats/io.go
+++ stats/io.go
@@ -13,18 +13,44 @@ type StatsConn struct {
 func (s StatsConn) Read(b []byte) (n int, err error) {
 	n, err = s.Conn.Read(b)
 	s.CounterDown.Add(int64(n))
-	return n, err
+	return
 }
 
 func (s StatsConn) Write(b []byte) (n int, err error) {
 	n, err = s.Conn.Write(b)
 	s.CounterUp.Add(int64(n))
-	return n, err
+	return
 }
 
 func NewStatsConn(c net.Conn) StatsConn {
 	return StatsConn{
 		Conn:        c,
+		CounterDown: DefaultManager.GetCounter(DownProxy),
+		CounterUp:   DefaultManager.GetCounter(UpProxy),
+	}
+}
+
+type StatsPacketConn struct {
+	net.PacketConn
+	CounterDown *Counter
+	CounterUp   *Counter
+}
+
+func (pc StatsPacketConn) ReadFrom(p []byte) (n int, addr net.Addr, err error) {
+	n, addr, err = pc.PacketConn.ReadFrom(p)
+	pc.CounterDown.Add(int64(n))
+	return
+}
+
+func (pc StatsPacketConn) WriteTo(p []byte, addr net.Addr) (n int, err error) {
+	n, err = pc.PacketConn.WriteTo(p, addr)
+	pc.CounterUp.Add(int64(n))
+	return
+}
+
+func NewStatsPacketConn(pc net.PacketConn) StatsPacketConn {
+	return StatsPacketConn{
+		PacketConn:  pc,
 		CounterDown: DefaultManager.GetCounter(DownProxy),
 		CounterUp:   DefaultManager.GetCounter(UpProxy),
 	}
