9f73f6a103e4280a7c0b05807eb3915a3119c801
xxf098 <ufxmeng@gmail.com>
2021-01-10 04:58:24 -0500

    decode safe

--- common/b64.go
+++ common/b64.go
@@ -1,8 +1,13 @@
 package common
 
-import "encoding/base64"
+import (
+	"encoding/base64"
+	"strings"
+)
 
 func DecodeB64(s string) (string, error) {
+	s = strings.ReplaceAll(s, "-", "+")
+	s = strings.ReplaceAll(s, "_", "/")
 	data, err := base64.StdEncoding.DecodeString(s)
 	if err != nil {
 		if data, err = base64.RawStdEncoding.DecodeString(s); err != nil {
