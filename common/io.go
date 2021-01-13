66fd84ac0a5d86f57944d2ddb8da4cecdcff7d6d
xxf098 <ufxmeng@gmail.com>
2021-01-12 20:37:13 -0500

    CopyBuffer

--- common/io.go
+++ common/io.go
@@ -117,3 +117,31 @@ type BufferdConn struct {
 func (c *BufferdConn) Read(b []byte) (int, error) {
 	return c.Br.Read(b)
 }
+
+func CopyBuffer(dst io.Writer, src io.Reader, buf []byte) (written int64, err error) {
+
+	for {
+		nr, er := src.Read(buf)
+		if nr > 0 {
+			nw, ew := dst.Write(buf[0:nr])
+			if nw > 0 {
+				written += int64(nw)
+			}
+			if ew != nil {
+				err = ew
+				break
+			}
+			if nr != nw {
+				err = io.ErrShortWrite
+				break
+			}
+		}
+		if er != nil {
+			if er != io.EOF {
+				err = er
+			}
+			break
+		}
+	}
+	return written, err
+}
