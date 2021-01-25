6769a7f1b5b6a6cb21bf80802a1e6403bcfb4472
xxf098 <ufxmeng@gmail.com>
2021-01-24 21:05:29 -0500

    unit

--- download/download.go
+++ download/download.go
@@ -41,14 +41,14 @@ func (e *Discard) Size() int64 {
 func ByteCountIEC(b int64) string {
 	const unit = 1024
 	if b < unit {
-		return fmt.Sprintf("%d B/S", b)
+		return fmt.Sprintf("%d B/s", b)
 	}
 	div, exp := int64(unit), 0
 	for n := b / unit; n >= unit; n /= unit {
 		div *= unit
 		exp++
 	}
-	return fmt.Sprintf("%.1f%cB/S",
+	return fmt.Sprintf("%.1f%cB/s",
 		float64(b)/float64(div), "KMGTPE"[exp])
 }
 
