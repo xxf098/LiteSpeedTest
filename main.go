9a14866954e5f83cd3186e6c92a7ad7f0e62926d
xxf098 <ufxmeng@gmail.com>
2021-02-25 11:01:50 +0800

    add --web option

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"flag"
+
+	webServer "github.com/xxf098/lite-proxy/web"
 )
 
 var (
 	link = flag.String("link", "", "proxy link")
 	port = flag.Int("port", 8090, "local port")
+	web  = flag.Bool("web", false, "start web")
 )
 
 func main() {
 	flag.Parse()
+	if *web {
+		webServer.ServeFile()
+		return
+	}
 	if *link == "" {
 		return
 	}
