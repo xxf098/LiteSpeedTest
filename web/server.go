f79edd8d00306139970485890aa56ed89d74e323
xxf098 <ufxmeng@gmail.com>
2021-02-25 13:44:23 +0800

    websocket server

---
+++ web/server.go
@@ -0,0 +1,56 @@
+package web
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{}
+
+func ServeFile() {
+	http.Handle("/", http.FileServer(http.Dir("web/gui/")))
+	http.HandleFunc("/test", updateTest)
+	fmt.Println("Start server at http://127.0.0.1:10871")
+	http.ListenAndServe(":10871", nil)
+}
+
+func updateTest(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, getMsgByte("started"))
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
+
+type Message struct {
+	ID   int    `json:"id"`
+	Info string `json:"info"`
+}
+
+func getMsgByte(typ string) []byte {
+	msg := Message{}
+	switch typ {
+	case "started", "fetchingsub", "begintest":
+		msg.Info = typ
+	}
+	b, _ := json.Marshal(msg)
+	return b
+}
