7f39f88bbf07aca40f329240b9865f76202acff7
xxf098 <ufxmeng@gmail.com>
2021-03-01 09:40:56 +0800

    DEFAULT_GROUP

--- web/msg.go
+++ web/msg.go
@@ -1,5 +1,9 @@
 package web
 
 const (
+	TROJAN_DEFAULT_GROUP    = "TrojanProvider"
+	V2RAY_DEFAULT_GROUP     = "V2RayProvider"
+	SS_DEFAULT_GROUP        = "SSProvider"
+	SSR_DEFAULT_GROUP       = "SSRProvider"
 	SPEEDTEST_ERROR_NONODES = "{\"info\":\"error\",\"reason\":\"nonodes\"}\n"
 )
