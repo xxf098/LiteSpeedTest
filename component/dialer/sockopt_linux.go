7558ce6007490f95cc03b04d3ff48422d91f2477
xxf098 <ufxmeng@gmail.com>
2021-03-05 14:14:55 +0800

    NewError

--- component/dialer/sockopt_linux.go
+++ component/dialer/sockopt_linux.go
@@ -1,13 +1,14 @@
 package dialer
 
 import (
-	"syscall"
+	"github.com/xxf098/lite-proxy/common"
 	"golang.org/x/sys/unix"
-	)
+	"syscall"
+)
 
 func setReusePort(fd uintptr) error {
 	if err := syscall.SetsockoptInt(int(fd), syscall.SOL_SOCKET, unix.SO_REUSEPORT, 1); err != nil {
-		return newError("failed to set SO_REUSEPORT").Base(err).AtWarning()
+		return common.NewError("failed to set SO_REUSEPORT").Base(err)
 	}
 	return nil
 }
