4f98a5f5fed3fd0bba43c9cc25ede3a28fe0831f
xxf098 <ufxmeng@gmail.com>
2021-04-07 09:27:06 -0400

    support posix

--- utils/status/writer_posix.go
+++ utils/status/writer_posix.go
@@ -1,1 +1,12 @@
 package status
+
+import (
+	"fmt"
+	"strings"
+)
+
+var clear = fmt.Sprintf("%c[%dA%c[2K", ESC, 1, ESC)
+
+func (w *Writer) clearLines() {
+	_, _ = fmt.Fprint(w.Out, strings.Repeat(clear, w.lineCount))
+}
