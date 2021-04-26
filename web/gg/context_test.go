65e0b1622c70462c10d815b55c6aa5840d2d36c7
xxf098 <ufxmeng@gmail.com>
2021-04-26 10:48:51 -0400

    add graph lib

---
+++ web/gg/context_test.go
@@ -0,0 +1,40 @@
+package gg
+
+import (
+	"math/rand"
+	"testing"
+)
+
+func TestDrawLine(t *testing.T) {
+	const W = 1024
+	const H = 1024
+	dc := NewContext(W, H)
+	dc.SetRGB(0, 0, 0)
+	dc.Clear()
+	for i := 0; i < 1000; i++ {
+		x1 := rand.Float64() * W
+		y1 := rand.Float64() * H
+		x2 := rand.Float64() * W
+		y2 := rand.Float64() * H
+		r := rand.Float64()
+		g := rand.Float64()
+		b := rand.Float64()
+		a := rand.Float64()*0.5 + 0.5
+		w := rand.Float64()*4 + 1
+		dc.SetRGBA(r, g, b, a)
+		dc.SetLineWidth(w)
+		dc.DrawLine(x1, y1, x2, y2)
+		dc.Stroke()
+	}
+	dc.SavePNG("out.png")
+}
+
+func TestText(t *testing.T) {
+	const S = 100
+	dc := NewContext(S, S)
+	dc.SetRGB(1, 1, 1)
+	dc.Clear()
+	dc.SetRGB(0, 0, 0)
+	dc.DrawStringAnchored("abcd1234", S/2, S/2, 0.5, 0.5)
+	dc.SavePNG("out.png")
+}
