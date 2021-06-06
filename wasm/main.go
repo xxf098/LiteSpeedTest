8da54d211ffaba0e17e35f1530ec7585022e2b75
xxf098 <ufxmeng@gmail.com>
2021-06-06 04:54:37 -0400

    load wasm

--- wasm/main.go
+++ wasm/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"strings"
+	"syscall/js"
+)
+
+func printMessage(this js.Value, inputs []js.Value) interface{} {
+	callback := inputs[len(inputs)-1]
+	message := inputs[0].String()
+	callback.Invoke(js.Null(), strings.ToUpper(message))
+	return nil
+}
 
 func main() {
-	fmt.Println("Hello, WebAssembly!")
+	js.Global().Set("printMessage", js.FuncOf(printMessage))
+	<-make(chan bool)
 }
