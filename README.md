b844d420d06ac6bfe25ceb76dc87ffc3a06a2d60
xxf098 <ufxmeng@gmail.com>
2021-06-11 21:38:05 -0400

    parse args

--- README.md
+++ README.md
@@ -6,12 +6,12 @@ LiteSpeedTest is a simple tool for batch test ss/ssr/v2ray/trojan servers.
 ```
 Run as speed test tool:
     ./lite
-    ./lite -port 10889
+    ./lite -p 10889
 
 Run as http/socks5 proxy:
     ./lite vmess://aHR0cHM6Ly9naXRodWIuY29tL3h4ZjA5OC9MaXRlU3BlZWRUZXN0
     ./lite ssr://aHR0cHM6Ly9naXRodWIuY29tL3h4ZjA5OC9MaXRlU3BlZWRUZXN0
-    ./lite -port 8091 vmess://aHR0cHM6Ly9naXRodWIuY29tL3h4ZjA5OC9MaXRlU3BlZWRUZXN0
+    ./lite -p 8091 vmess://aHR0cHM6Ly9naXRodWIuY29tL3h4ZjA5OC9MaXRlU3BlZWRUZXN0
 ```
 
 ### Build
--- main.go
+++ main.go
@@ -10,47 +10,38 @@ import (
 )
 
 var (
-	link = flag.String("link", "", "add subscription link")
-	port = flag.Int("port", 8090, "set port")
-	test = flag.Bool("test", false, "start batch test")
+	// link = flag.String("link", "", "add subscription link")
+	port = flag.Int("p", 8090, "set port")
+	// test = flag.Bool("test", false, "start batch test")
 )
 
 func main() {
 	flag.Parse()
-	if (*test || len(os.Args) < 2) && *link == "" {
-		err := webServer.ServeFile(10888)
-		if err != nil {
+	link := ""
+	for _, arg := range os.Args {
+		if _, err := utils.CheckLink(arg); err == nil {
+			link = arg
+			break
+		}
+	}
+	if link == "" {
+		if len(os.Args) < 2 {
+			*port = 10888
+		}
+		if err := webServer.ServeFile(*port); err != nil {
 			log.Fatalln(err)
 		}
 		return
 	}
-	if *link == "" {
-		if len(os.Args) > 1 {
-			for i := 1; i < len(os.Args); i++ {
-				arg := os.Args[i]
-				if _, err := utils.CheckLink(arg); err == nil {
-					link = &arg
-					break
-				}
-			}
-
-		}
-		if *link == "" {
-			err := webServer.ServeFile(*port)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			return
-		}
-	}
 	c := Config{
 		LocalHost: "127.0.0.1",
 		LocalPort: *port,
-		Link:      *link,
+		Link:      link,
 	}
-	p, err := startInstance(c)
-	if err != nil {
+	if p, err := startInstance(c); err != nil {
 		log.Fatalln(err)
+	} else {
+		p.Run()
 	}
-	p.Run()
+
 }
