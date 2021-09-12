a5ea375ba1555c9b51328fdc721467f085b80774
xxf098 <ufxmeng@gmail.com>
2021-09-12 10:37:14 -0400

    TestNewID

---
+++ transport/vmess/user_test.go
@@ -0,0 +1,20 @@
+package vmess
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/gofrs/uuid"
+)
+
+func TestNewID(t *testing.T) {
+	uid, err := uuid.FromString("803277e5-64ef-48f9-8fd5-de82481ee789")
+	if err != nil {
+		t.Error(err)
+	}
+	newid := newID(&uid)
+	ids := newAlterIDs(newid, 1)
+	for _, id := range ids {
+		fmt.Println(id.UUID.String())
+	}
+}
