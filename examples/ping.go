5e4924e45bf4aad17d972a47d4f8fb6cbbe1a271
xxf098 <ufxmeng@gmail.com>
2022-10-01 09:00:21 -0400

    add async api

--- examples/ping.go
+++ examples/ping.go
@@ -9,7 +9,6 @@ import (
 )
 
 func main() {
-	ctx := context.Background()
 	link := "vmess://aHR0cHM6Ly9naXRodWIuY29tL3h4ZjA5OC9MaXRlU3BlZWRUZXN0"
 	opts := web.ProfileTestOptions{
 		GroupName:       "Default",
@@ -25,16 +24,36 @@ func main() {
 		Timeout:         10 * time.Second,
 		GeneratePicMode: 0, // 0: base64 1:file path 2: no pic
 	}
-	nodes, err := web.TestContext(ctx, opts, &web.EmptyMessageWriter{})
+	ctx := context.Background()
+	pingSync(ctx, opts)
+	// pingAsync
+}
 
+func pingSync(ctx context.Context, opts web.ProfileTestOptions) error {
+	nodes, err := web.TestContext(ctx, opts, &web.EmptyMessageWriter{})
 	if err != nil {
-		return
+		return err
 	}
 
 	for _, node := range nodes {
+		// tested node info here
+		if node.IsOk {
+			fmt.Println(node.Remarks, node.Ping, node.Link)
+		}
+	}
+	return nil
+}
+
+func pingAsync(ctx context.Context, opts web.ProfileTestOptions) error {
+	nodeChan, err := web.TestAsyncContext(ctx, opts)
+	if err != nil {
+		return err
+	}
+	for node := range nodeChan {
 		// process node info here
 		if node.IsOk {
 			fmt.Println(node.Remarks, node.Ping, node.Link)
 		}
 	}
+	return nil
 }
