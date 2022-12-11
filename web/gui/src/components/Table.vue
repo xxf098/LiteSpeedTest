5780008987df24d307f0eddb6554b34c0423e148
xxf098 <ufxmeng@gmail.com>
2022-12-11 10:07:18 -0500

    add table

---
+++ web/gui/src/components/Table.vue
@@ -0,0 +1,68 @@
+<template>
+  <el-table-v2
+    :columns="columns"
+    :data="result"
+    :width="1700"
+    :height="800"
+    fixed
+  />
+</template>
+
+<script>
+export default {
+  props: ['result']
+}
+</script>
+
+<script setup>
+
+let id = 0
+
+const columns = [
+  {
+    key: "remark",
+    title: 'Remark',
+    dataKey: 'remark',
+    width: 800,
+    align: "center",
+  },
+  {
+    key: "server",
+    title: 'Server',
+    dataKey: 'server',
+    width: 300,
+    align: "center",
+  },
+  {
+    key: "protocol",
+    title: 'Protocol',
+    dataKey: 'protocol',
+    width: 120,
+    align: "center",
+  },
+  {
+    key: "ping",
+    title: 'Ping',
+    dataKey: 'ping',
+    width: 100,
+    align: "center",
+  },
+  {
+    key: "speed",
+    title: 'AvgSpeed',
+    dataKey: 'speed',
+    width: 180,
+    align: "center",
+  },
+  {
+    key: "maxspeed",
+    title: 'MaxSpeed',
+    dataKey: 'maxspeed',
+    width: 180,
+    align: "center",
+  },
+]
+
+</script>
+
+
