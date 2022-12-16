660c39a3da1ddbd19804b9761667ac36cdc79488
xxf098 <ufxmeng@gmail.com>
2022-12-16 01:17:12 -0500

    fix wasm_exec.js

--- web/gui/vite.config.js
+++ web/gui/vite.config.js
@@ -10,10 +10,5 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  build: {
-    rollupOptions: {
-      external: ["wasm_exec.js"]
-    }
   }
 })
